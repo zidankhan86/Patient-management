a8ae3e6774e6847fd22af9ab99b4d5a16b691d42
zidankhan86 <zidankhan718@gmail.com>
2024-09-18 14:13:22 +0600

    fix: report design

--- resources/views/backend/pages/report/report.blade.php
+++ resources/views/backend/pages/report/report.blade.php
@@ -2,81 +2,76 @@
 
 @section('content')
     <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card">
+        <div class="row justify-content-center">
+            <div class="col-lg-10">
+                <div class="card shadow-sm">
                     <div class="card-body">
-                        <div class="card-title">
+                        <div class="text-center mb-4">
+                            <h1 class="card-title">Appointment Report</h1>
+                            <hr class="w-25 mx-auto">
+                        </div>
 
-
-                            <br>
-                            <h1>Appointment Report</h1><br>
-
-                            <form action="{{ route('order.report.search') }}" method="get">
-
-                                <div class="container">
-                                    <div class="row align-items-end">
-                                        <div class="col-md-4">
-                                            <div class="form-group">
-                                                <label for="from_date">From date:</label>
-                                                <input name="from_date" type="date" class="form-control" id="from_date">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <div class="form-group">
-                                                <label for="to_date">To date:</label>
-                                                <input name="to_date" type="date" class="form-control" id="to_date">
-                                            </div>
-                                        </div>
-                                        <div class="col-md-2">
-                                            <button type="submit" class="btn btn-success btn-block">Search</button>
-                                        </div>
+                        <form action="{{ route('order.report.search') }}" method="get" class="mb-4">
+                            <div class="row">
+                                <div class="col-md-5">
+                                    <div class="form-group">
+                                        <label for="from_date">From Date:</label>
+                                        <input name="from_date" type="date" class="form-control" id="from_date">
                                     </div>
                                 </div>
-
-                            </form>
-                            <div id="orderReport">
-
-                                <h1>Appointment Reports- {{ date('Y-m-d') }}</h1><br>
-                                <table class="table table-striped">
-                                    <thead>
-                                        <tr>
-
-                                            <th scope="col">Serial</th>
-                                            <th scope="col">Patient Name</th>
-                                            <th scope="col">Doctor Name</th>
-                                            <th scope="col">Date & Time</th>
-                                            <th scope="col">Type</th>
-                                            <th scope="col">Status</th>
-
-
-
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-
-
-                                        @if (isset($Appointment))
-                                        @forelse ($Appointment as $index => $appointment)
-                                        <tr>
-                                            <td>{{ $index + 1 }}</td>
-                                            <td>{{ $appointment->patient->name }}</td>
-                                            <td>{{ $appointment->doctor->name }}</td>
-                                            <td>{{ \Carbon\Carbon::parse($appointment->appointment_date)->format('d M Y h:i A') }}</td>
-                                            <td>{{ $appointment->type }}</td>
-                                            <td>{{ $appointment->status }}</td>
-                                            
-                                        </tr>
-                                    @empty
-                                        <tr>
-                                            <td colspan="7" class="text-center">No appointments available</td>
-                                        </tr>
-                                    @endforelse
-                                        @endif
-                                    </tbody>
-                                </table>
+                                <div class="col-md-5">
+                                    <div class="form-group">
+                                        <label for="to_date">To Date:</label>
+                                        <input name="to_date" type="date" class="form-control" id="to_date">
+                                    </div>
+                                </div>
+                                <div class="col-md-2 d-flex align-items-end">
+                                    <button type="submit" class="btn btn-success w-100">Search</button>
+                                </div>
                             </div>
-                            <button onclick="printDiv('orderReport')" class="btn btn-success">Print</button>
+                        </form>
+
+                        <div id="orderReport">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <h2>Appointment Reports - {{ date('Y-m-d') }}</h2>
+                                <button onclick="printDiv('orderReport')" class="btn btn-outline-primary">
+                                    <i class="fas fa-print"></i> Print Report
+                                </button>
+                            </div>
+                            <table class="table table-bordered table-hover mt-3">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Patient Name</th>
+                                        <th scope="col">Doctor Name</th>
+                                        <th scope="col">Date & Time</th>
+                                        <th scope="col">Type</th>
+                                        <th scope="col">Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @if (isset($Appointment))
+                                        @forelse ($Appointment as $index => $appointment)
+                                            <tr>
+                                                <td>{{ $index + 1 }}</td>
+                                                <td>{{ $appointment->patient->name }}</td>
+                                                <td>{{ $appointment->doctor->name }}</td>
+                                                <td>{{ \Carbon\Carbon::parse($appointment->appointment_date)->format('d M Y h:i A') }}</td>
+                                                <td>{{ ucfirst($appointment->type) }}</td>
+                                                <td>
+                                                    <span class="badge badge-{{ $appointment->status == 'Completed' ? 'success' : 'warning' }}">
+                                                        {{ $appointment->status }}
+                                                    </span>
+                                                </td>
+                                            </tr>
+                                        @empty
+                                            <tr>
+                                                <td colspan="6" class="text-center">No appointments available</td>
+                                            </tr>
+                                        @endforelse
+                                    @endif
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
@@ -93,5 +88,4 @@
             document.body.innerHTML = originalContents;
         }
     </script>
-
 @endsection
